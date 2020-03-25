c901aa07aa76b1e1324636d284a14fb2152bf8f7
和田健嗣 <ponporopen@gmail.com>
2020-03-25 15:47:16 +0900

    initial commit

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <h1>About</h1>
+    <a href="/">To Index</a>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'about',
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <h1>hello</h1>
+    <a href="/about">To About</a>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'index',
+}
+</script>
