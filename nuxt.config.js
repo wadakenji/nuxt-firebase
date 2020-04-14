64d0530b16dafb2755809904af681641b59379cf
和田健嗣 <ponporopen@gmail.com>
2020-04-14 19:58:07 +0900

    pwa対応

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,40 @@ export default {
     ],
   },
   plugins: ['@/plugins/firebase'],
-  modules: ['@nuxtjs/dotenv', ['@nuxtjs/moment', ['ja']]],
+  modules: [
+    '@nuxtjs/dotenv',
+    ['@nuxtjs/moment', ['ja']],
+    '@nuxtjs/pwa',
+    '@nuxtjs/onesignal',
+  ],
+  pwa: {
+    meta: {
+      name: 'コロふん',
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      description: 'コロナによって分報をすることになった俺たち',
+      lang: 'ja',
+    },
+    manifest: {
+      name: 'コロふん',
+      short_name: 'コロふん',
+      title: 'コロふん',
+      'og:title': 'コロふん',
+      description: 'コロナによって分報をすることになった俺たち',
+      'og:description': 'コロナによって分報をすることになった俺たち',
+      lang: 'ja',
+      theme_color: '#529b58',
+      background_color: '#bde0c0',
+      display: 'standalone',
+      scope: '/',
+      start_url: '/',
+    },
+  },
+  oneSignal: {
+    init: {
+      appId: 'e8bd7120-2ae3-4c1e-a946-8a0a6f022f7e',
+      allowLocalhostAsSecureOrigin: true,
+    },
+  },
   buildModules: ['@nuxtjs/vuetify'],
 }
---
+++ static/OneSignalSDKWorker.js
@@ -0,0 +1,1 @@
+importScripts('/sw.js?1586860637913', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
